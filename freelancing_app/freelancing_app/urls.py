c2a3f45746577174f3402e3c2b93d5b72434cdb7
bkash-gg <np01cp4s230121@islingtoncollege.edu.np>
2024-12-17 12:59:31 +0545

    Static files managed

--- freelancing_app/freelancing_app/urls.py
+++ freelancing_app/freelancing_app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("home.urls", namespace="homes")),
@@ -7,3 +9,6 @@ urlpatterns = [
 ]
 
 handler404 = 'home.views.handling_404'
+
+if settings.DEBUG:  
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
